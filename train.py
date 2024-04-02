c6b96b28bad48749131b4e4b8d8e126656c91d12
Oskar <63914721+Skadoink@users.noreply.github.com>
2024-04-02 17:51:23 +1300

    yaml and train revised

--- train.py
+++ train.py
@@ -1,17 +1,12 @@
 from ultralytics import YOLO
-from PIL import Image
-import os
-import cv2
-import numpy as np
-
-"""YOLOv8 Train seabird object detection model"""
-
-# Load YOLO
-model = YOLO("yolov8s.pt")
-
-# Load images
-path = "data/images/train"
-images = [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".jpg")]
-
-# Train
-results = model.train(images, batch_size=8, epochs=10, weights="yolov8s.pt")
+ 
+# Load the model.
+model = YOLO('yolov8s.pt')
+ 
+# Training.
+results = model.train(
+   data='seaeyes.yaml',
+   imgsz=640,
+   epochs=10,
+   batch=8,
+   name='yolov8s_10e')
