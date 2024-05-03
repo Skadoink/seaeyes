a5c4bc1dcb74e2b1dd600c8b1784c4f61e4ca9dd
Oskar <oskaege@gmail.com>
2024-05-03 22:25:15 +1200

    swag

--- train.py
+++ train.py
@@ -1,12 +1,20 @@
 from ultralytics import YOLO
  
+#Setup GPU
+import torch
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+print(device)
+
 # Load the model.
 model = YOLO('yolov8s.pt')
  
 # Training.
 results = model.train(
    data='seaeyes.yaml',
-   imgsz=640,
+   imgsz=2000,
    epochs=10,
-   batch=8,
-   name='yolov8s_10e')
+   batch=1,
+   name='yolov8s_10e',
+   workers = 0)
+
+results = model.val()
