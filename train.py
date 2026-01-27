5a6c4dc8df18f7cbee7b683dcca974aff6b8bf02
Oskar <63914721+Skadoink@users.noreply.github.com>
2026-01-27 15:12:29 +1300

    Update configuration and training script for YOLO model

--- train.py
+++ train.py
@@ -1,22 +1,32 @@
-from ultralytics import YOLO
- 
+print("starting")
+import cv2
+from ultralytics import YOLO 
 #Setup GPU
 import torch
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 print(device)
 
 # Load the model.
-model = YOLO('yolov8s.pt')
- 
-# Training.
+model = YOLO('yolo26s.pt')
+# Training with default parameters:
 results = model.train(
    data='seaeyes.yaml',
+   epochs=10,
    imgsz=640,
-   epochs=20,
-   batch=16,
-   name='yolov8s_10e',
-   device = device,
-   workers = 0,
-   plots = True)
+   project="./runs"
+)
+print("trained")
 
-results = model.val()
+# # Training.
+# results = model.train(
+#    data='seaeyes.yaml',
+#    imgsz=640,
+#    epochs=20,
+#    batch=16,
+#    name='yolov8s_10e',
+#    device = device,
+#    workers = 0,
+#    plots = True)
+
+results = model.val(project="./runs")
+print("validated")
